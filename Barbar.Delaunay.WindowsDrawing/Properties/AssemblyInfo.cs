49a81593380a4387dd49b3090a3b401975f0080e
svejdo1 <svejdo1@gmail.com>
2017-02-08 16:32:44 +0100

    dev

--- Barbar.Delaunay.WindowsDrawing/Properties/AssemblyInfo.cs
+++ Barbar.Delaunay.WindowsDrawing/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Barbar.Delaunay.WindowsDrawing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sample renderer for polygon map generator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Barbar.Delaunay.WindowsDrawing")]
--- Barbar.Delaunay/Properties/AssemblyInfo.cs
+++ Barbar.Delaunay/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Barbar.Delaunay")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Polygon map terrain generator, written in C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Barbar.Delaunay")]
